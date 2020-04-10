b5aeac46e8351c3075f16a0b6882ca1c9a0bb888
OlegFox773 <60777066+OlegFox773@users.noreply.github.com>
2020-04-11 00:49:42 +0700

    open_web-suite

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const cardButton = document.querySelector("#card_button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle("is_open");
+}
+
+new WOW().init();
